1cb1cdbba8ce72ab1815d99fb6b4833c829d3693
Jaime Olivares <jolivares@hotmail.com>
2019-04-11 12:29:11 -0700

    Completed all unit tests

--- test/CanTranscodeUnitTest.cs
+++ test/CanTranscodeUnitTest.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using Dicom;
@@ -14,67 +15,23 @@ namespace Efferent.Native.Test
             TranscoderManager.SetImplementation(new Efferent.Native.Codec.NativeTranscoderManager());
         }
 
-        [TestMethod]
-        public void CanTranscodeRLELossless()
+        [DataTestMethod]
+        [DataRow("RLELossless")]
+        [DataRow("JPEG2000Lossless")]
+        [DataRow("JPEG2000Lossy")]
+        [DataRow("JPEGProcess1")]
+        [DataRow("JPEGProcess2_4")]
+        [DataRow("JPEGProcess14")]
+        [DataRow("JPEGProcess14SV1")]
+        [DataRow("JPEGLSLossless")]
+        [DataRow("JPEGLSNearLossless")]
+        public void PerformCanTranscode(string name)
         {
-            Assert.IsTrue(TranscoderManager.CanTranscode(DicomTransferSyntax.ExplicitVRLittleEndian, DicomTransferSyntax.RLELossless));
-            Assert.IsTrue(TranscoderManager.CanTranscode(DicomTransferSyntax.RLELossless, DicomTransferSyntax.ExplicitVRLittleEndian));
-        }
+            const BindingFlags binding = BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy;
+            var ts = (DicomTransferSyntax)typeof(DicomTransferSyntax).GetField(name, binding).GetValue(0);
 
-        [TestMethod]
-        public void CanTranscodeJpeg2000Lossless()
-        {
-            Assert.IsTrue(TranscoderManager.CanTranscode(DicomTransferSyntax.ExplicitVRLittleEndian, DicomTransferSyntax.JPEG2000Lossless));
-            Assert.IsTrue(TranscoderManager.CanTranscode(DicomTransferSyntax.JPEG2000Lossless, DicomTransferSyntax.ExplicitVRLittleEndian));
-        }
-
-        [TestMethod]
-        public void CanTranscodeJpeg2000Lossy()
-        {
-            Assert.IsTrue(TranscoderManager.CanTranscode(DicomTransferSyntax.ExplicitVRLittleEndian, DicomTransferSyntax.JPEG2000Lossy));
-            Assert.IsTrue(TranscoderManager.CanTranscode(DicomTransferSyntax.JPEG2000Lossy, DicomTransferSyntax.ExplicitVRLittleEndian));
-        }
-
-        [TestMethod]
-        public void CanTranscodeJPEGProcess1()
-        {
-            Assert.IsTrue(TranscoderManager.CanTranscode(DicomTransferSyntax.ExplicitVRLittleEndian, DicomTransferSyntax.JPEGProcess1));
-            Assert.IsTrue(TranscoderManager.CanTranscode(DicomTransferSyntax.JPEGProcess1, DicomTransferSyntax.ExplicitVRLittleEndian));
-        }
-
-        [TestMethod]
-        public void CanTranscodeJPEGProcess2_4()
-        {
-            Assert.IsTrue(TranscoderManager.CanTranscode(DicomTransferSyntax.ExplicitVRLittleEndian, DicomTransferSyntax.JPEGProcess2_4));
-            Assert.IsTrue(TranscoderManager.CanTranscode(DicomTransferSyntax.JPEGProcess2_4, DicomTransferSyntax.ExplicitVRLittleEndian));
-        }
-
-        [TestMethod]
-        public void CanTranscodeJPEGProcess14()
-        {
-            Assert.IsTrue(TranscoderManager.CanTranscode(DicomTransferSyntax.ExplicitVRLittleEndian, DicomTransferSyntax.JPEGProcess14));
-            Assert.IsTrue(TranscoderManager.CanTranscode(DicomTransferSyntax.JPEGProcess14, DicomTransferSyntax.ExplicitVRLittleEndian));
-        }
-
-        [TestMethod]
-        public void CanTranscodeJPEGProcess14SV1()
-        {
-            Assert.IsTrue(TranscoderManager.CanTranscode(DicomTransferSyntax.ExplicitVRLittleEndian, DicomTransferSyntax.JPEGProcess14SV1));
-            Assert.IsTrue(TranscoderManager.CanTranscode(DicomTransferSyntax.JPEGProcess14SV1, DicomTransferSyntax.ExplicitVRLittleEndian));
-        }
-
-        [TestMethod]
-        public void CanTranscodeJPEGLSLossless()
-        {
-            Assert.IsTrue(TranscoderManager.CanTranscode(DicomTransferSyntax.ExplicitVRLittleEndian, DicomTransferSyntax.JPEGLSLossless));
-            Assert.IsTrue(TranscoderManager.CanTranscode(DicomTransferSyntax.JPEGLSLossless, DicomTransferSyntax.ExplicitVRLittleEndian));
-        }
-
-        [TestMethod]
-        public void CanTranscodeJPEGLSNearLossless()
-        {
-            Assert.IsTrue(TranscoderManager.CanTranscode(DicomTransferSyntax.ExplicitVRLittleEndian, DicomTransferSyntax.JPEGLSNearLossless));
-            Assert.IsTrue(TranscoderManager.CanTranscode(DicomTransferSyntax.JPEGLSNearLossless, DicomTransferSyntax.ExplicitVRLittleEndian));
+            Assert.IsTrue(TranscoderManager.CanTranscode(DicomTransferSyntax.ExplicitVRLittleEndian, ts));
+            Assert.IsTrue(TranscoderManager.CanTranscode(ts, DicomTransferSyntax.ExplicitVRLittleEndian));
         }
     }
 }
--- test/TranscodeUnitTest.cs
+++ test/TranscodeUnitTest.cs
@@ -13,6 +13,8 @@ namespace Efferent.Native.Test
     [TestClass]
     public class TranscodeUnitTest
     {
+        const BindingFlags binding = BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy;
+
         [TestInitialize]
         public void Initialization()
         {
@@ -25,20 +27,99 @@ namespace Efferent.Native.Test
         [DataRow("RLELossless")]
         [DataRow("JPEG2000Lossless")]
         [DataRow("JPEG2000Lossy")]
-        [DataRow("JPEGProcess1")]
-        [DataRow("JPEGProcess2_4")]
         [DataRow("JPEGProcess14")]
         [DataRow("JPEGProcess14SV1")]
         [DataRow("JPEGLSLossless")]
         [DataRow("JPEGLSNearLossless")]
-        public void PerformTranscode(string name)
+        [DataRow("JPEGProcess1")]
+        [DataRow("JPEGProcess2_4")]
+        public void PerformTranscode8bits(string name)
         {
-            var output = $"out/{name}.dcm";
-            var data = DicomFile.Open("test.dcm");
-            var binding = BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy;
+            var output = $"out/{name}8bits.dcm";
+            var data = DicomFile.Open("test8bits.dcm");
 
             var ts = (DicomTransferSyntax)typeof(DicomTransferSyntax).GetField(name, binding).GetValue(0);
             var image = new DicomFile(data.Dataset).Clone(ts);
+
+            Assert.IsNotNull(image);
+
+            image.Save(output);
+
+            var data1 = DicomFile.Open(output);
+            Assert.IsTrue(data1.Dataset.Contains(DicomTag.PixelData));
+        }
+
+        [DataTestMethod]
+        [DataRow("RLELossless")]
+        [DataRow("JPEG2000Lossless")]
+        [DataRow("JPEG2000Lossy")]
+        [DataRow("JPEGProcess14")]
+        [DataRow("JPEGProcess14SV1")]
+        [DataRow("JPEGLSLossless")]
+        [DataRow("JPEGLSNearLossless")]
+        [DataRow("JPEGProcess1")]
+        [DataRow("JPEGProcess2_4")]
+        public void InverseTranscode8bits(string name)
+        {
+            var input = $"out/{name}8bits.dcm";
+            var output = $"out/from_{name}8bits.dcm";
+            var data = DicomFile.Open(input);
+
+            var ts = (DicomTransferSyntax)typeof(DicomTransferSyntax).GetField(name, binding).GetValue(0);
+            var image = new DicomFile(data.Dataset).Clone(DicomTransferSyntax.ExplicitVRLittleEndian);
+
+            Assert.IsNotNull(image);
+
+            image.Save(output);
+
+            var data1 = DicomFile.Open(output);
+            Assert.IsTrue(data1.Dataset.Contains(DicomTag.PixelData));
+        }
+
+        [DataTestMethod]
+        [DataRow("RLELossless")]
+        [DataRow("JPEG2000Lossless")]
+        [DataRow("JPEG2000Lossy")]
+        [DataRow("JPEGProcess14")]
+        [DataRow("JPEGProcess14SV1")]
+        [DataRow("JPEGLSLossless")]
+        [DataRow("JPEGLSNearLossless")]
+        // [DataRow("JPEGProcess1")]    // Not passing as the input image is 16 bits
+        // [DataRow("JPEGProcess2_4")]  // Not passing as the input image is 16 bits
+        public void PerformTranscode16bits(string name)
+        {
+            var output = $"out/{name}16bits.dcm";
+            var data = DicomFile.Open("test16bits.dcm");
+
+            var ts = (DicomTransferSyntax)typeof(DicomTransferSyntax).GetField(name, binding).GetValue(0);
+            var image = new DicomFile(data.Dataset).Clone(ts);
+
+            Assert.IsNotNull(image);
+
+            image.Save(output);
+
+            var data1 = DicomFile.Open(output);
+            Assert.IsTrue(data1.Dataset.Contains(DicomTag.PixelData));
+        }
+
+        [DataTestMethod]
+        [DataRow("RLELossless")]
+        [DataRow("JPEG2000Lossless")]
+        [DataRow("JPEG2000Lossy")]
+        [DataRow("JPEGProcess14")]
+        [DataRow("JPEGProcess14SV1")]
+        [DataRow("JPEGLSLossless")]
+        [DataRow("JPEGLSNearLossless")]
+        // [DataRow("JPEGProcess1")]    // Not passing as the input image is 16 bits
+        // [DataRow("JPEGProcess2_4")]  // Not passing as the input image is 16 bits
+        public void InverseTranscode16bits(string name)
+        {
+            var input = $"out/{name}16bits.dcm";
+            var output = $"out/from_{name}16bits.dcm";
+            var data = DicomFile.Open(input);
+
+            var ts = (DicomTransferSyntax)typeof(DicomTransferSyntax).GetField(name, binding).GetValue(0);
+            var image = new DicomFile(data.Dataset).Clone(DicomTransferSyntax.ExplicitVRLittleEndian);
 
             Assert.IsNotNull(image);
 
