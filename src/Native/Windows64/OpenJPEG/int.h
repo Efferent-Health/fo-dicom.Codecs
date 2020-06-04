c4caf6e9159e1b8eeb1207dc36ce44986cde4e0c
cbeltran1306 <christian13694@gmail.com>
2020-06-04 10:50:24 -0500

    Final changes for JPEG2000

--- src/Native/Windows64/OpenJPEG/int.h
+++ src/Native/Windows64/OpenJPEG/int.h
@@ -47,14 +47,14 @@ The functions in INT.H have for goal to realize operations on integers.
 #ifdef HAVE_STDINT_H
 #else
 #if defined(_WIN32)
-typedef   signed __int8   int8_t
-typedef unsigned __int8   uint8_t
-typedef   signed __int16  int16_t
-typedef unsigned __int16  uint16_t
-typedef   signed __int32  int32_t
-typedef unsigned __int32  uint32_t
-typedef   signed __int64  int64_t
-typedef unsigned __int64  uint64_t
+typedef   signed __int8   int8_t;
+typedef unsigned __int8   uint8_t;
+typedef   signed __int16  int16_t;
+typedef unsigned __int16  uint16_t;
+typedef   signed __int32  int32_t;
+typedef unsigned __int32  uint32_t;
+typedef   signed __int64  int64_t;
+typedef unsigned __int64  uint64_t;
 #elif defined(__linux__)
 	#include <inttypes.h>
 #elif defined(__APPLE__)
