15205bbcb073ae07f80b1d8083ba193f10d16c4d
cbeltran1306 <christian13694@gmail.com>
2020-06-04 10:41:24 -0500

    Fixes for decode JPEG2000

--- src/Native/Windows64/OpenJPEG/int.h
+++ src/Native/Windows64/OpenJPEG/int.h
@@ -45,19 +45,23 @@ The functions in INT.H have for goal to realize operations on integers.
 /* ----------------------------------------------------------------------- */
 
 #ifdef HAVE_STDINT_H
-#include <stdint.h>
 #else
 #if defined(_WIN32)
-typedef   signed __int8   int8_t;
-typedef unsigned __int8   uint8_t;
-typedef   signed __int16  int16_t;
-typedef unsigned __int16  uint16_t;
-typedef   signed __int32  int32_t;
-typedef unsigned __int32  uint32_t;
-typedef   signed __int64  int64_t;
-typedef unsigned __int64  uint64_t;
-#else
-#error unsupported platform
+typedef   signed __int8   int8_t
+typedef unsigned __int8   uint8_t
+typedef   signed __int16  int16_t
+typedef unsigned __int16  uint16_t
+typedef   signed __int32  int32_t
+typedef unsigned __int32  uint32_t
+typedef   signed __int64  int64_t
+typedef unsigned __int64  uint64_t
+#elif defined(__linux__)
+	#include <inttypes.h>
+#elif defined(__APPLE__)
+	#include "TargetConditionals.h"
+    #ifdef TARGET_OS_MAC
+		#include <inttypes.h>
+    #endif
 #endif
 #endif
 
