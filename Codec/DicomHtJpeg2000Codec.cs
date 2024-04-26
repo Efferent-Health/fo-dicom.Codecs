944db2ee46ad30ebe0d64ead6c6071e77531cbcb
Christian Beltran <cbeltran@efferenthealth.com>
2024-04-26 14:14:20 -0500

    minor changes

--- Codec/DicomHtJpeg2000Codec.cs
+++ Codec/DicomHtJpeg2000Codec.cs
@@ -100,15 +100,19 @@ namespace FellowOakDicom.Imaging.NativeCodec
 
     public abstract class DicomHtJpeg2000NativeCodec : DicomHtJpeg2000Codec
     {   
+        // Encode HTJ2K for win_x64
         [DllImport("Dicom.Native.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall, EntryPoint = "InvokeHTJ2KEncode")]
         public static extern unsafe void InvokeHTJ2KEncode_winx64(ref Htj2k_outdata j2c_outinfo, byte* source, uint sourceLength, ref Frameinfo frameinfo, OPJ_PROG_ORDER progressionOrder = OPJ_PROG_ORDER.PROG_UNKNOWN);
 
+        // Decode HTJ2K for win_x64
         [DllImport("Dicom.Native.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall, EntryPoint = "InvokeHTJ2KDecode")]
         public static extern unsafe void InvokeHTJ2KDecode_winx64(ref Raw_outdata raw_outinfo, byte* source, uint sourceLength);
 
+        // Encode HTJ2k
         [DllImport("Dicom.Native", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall, EntryPoint = "InvokeHTJ2KEncode")]
         public static extern unsafe void InvokeHTJ2KEncode(ref Htj2k_outdata j2c_outinfo, byte* source, uint sourceLength, ref Frameinfo frameinfo, OPJ_PROG_ORDER progressionOrder = OPJ_PROG_ORDER.PROG_UNKNOWN);
 
+        // Decode HTJ2k
         [DllImport("Dicom.Native", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall, EntryPoint = "InvokeHTJ2KDecode")]
         public static extern unsafe void InvokeHTJ2KDecode(ref Raw_outdata raw_outinfo, byte* source, uint sourceLength);
 
